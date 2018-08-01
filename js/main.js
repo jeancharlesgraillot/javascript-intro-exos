7646be299b28c66cb358f7c36c551e41807918d7
jeancharlesgraillot <graillot.jc@gmail.com>
2018-08-01 09:36:47 +0200

    beginners introduction finished

---
+++ js/main.js
@@ -0,0 +1,45 @@
+//Exo 1//
+
+/*console.log("Hello World !");
+
+var hello = ("Hello World !");
+
+console.log(hello);*/
+
+
+//Exo 2//
+
+/*console.log(3*3);
+console.log(12/0);
+console.log(4+9+78);
+console.log(12-7);
+console.log(5e4);*/
+
+//Exo 3//
+
+/*var nom = prompt("Entrez votre nom :");
+var res = nom.toUpperCase ();
+console.log("Bonjour Mr ou Mme " + res + " !");*/
+
+//Exo 4//
+
+/*var nom = prompt("Entrez votre nom :");
+var prenom = prompt("Entrez votre prenom :");
+var nomUp = nom.toUpperCase();
+var prenomUp = prenom.toUpperCase();
+console.log("Bonjour " + prenomUp + " " + nomUp + " !");*/
+
+//Exo 5//
+
+/*myNumber = "123";
+console.log(myNumber);
+myNumber = Number("123");
+console.log(myNumber);*/
+
+//Exo 6//
+
+/*var name = "Jean-Charles Graillot";
+var upperCaseName = name.toUpperCase();
+var lowerCaseName = name.toLowerCase();
+console.log(upperCaseName);
+console.log(lowerCaseName);*/

47a037448179dd865c694be440da45a0566be317
jeancharlesgraillot <graillot.jc@gmail.com>
2018-08-01 11:31:38 +0200

    advanced introduction finished

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+/***********************************Beginners Intro****************************/
+
+
 //Exo 1//
 
 /*console.log("Hello World !");
@@ -32,9 +35,9 @@ console.log("Bonjour " + prenomUp + " " + nomUp + " !");*/
 //Exo 5//
 
 /*myNumber = "123";
-console.log(myNumber);
 myNumber = Number("123");
-console.log(myNumber);*/
+console.log(myNumber);
+console.log(typeof myNumber);*/
 
 //Exo 6//
 
@@ -43,3 +46,76 @@ var upperCaseName = name.toUpperCase();
 var lowerCaseName = name.toLowerCase();
 console.log(upperCaseName);
 console.log(lowerCaseName);*/
+
+
+/*************************************Intro advanced***************************/
+
+
+//Exo 1//
+
+/*var string1 = "Vous êtes trop fort";
+var string2 = "";
+
+if (string1.length === 0) {
+  console.log("false");
+}else {
+  console.log("true");
+}
+
+if (string2.length === 0) {
+  console.log("false");
+}else {
+  console.log("true");
+}*/
+
+//Exo 2//
+
+/*var actualYear = Number(prompt("Entrez l'année actuelle :"));
+var birthYear = Number(prompt("Entrez votre année de naissance :"));
+var agePerso = actualYear - birthYear;
+
+console.log("Votre âge est de " + agePerso + " ans");
+
+var neighborAge = Number(prompt("Entrez l'âge du voisin"));
+var twoAges = agePerso + neighborAge;
+
+console.log("Vos âges cumulés sont de " + twoAges + " ans");*/
+
+//Exo 3//
+
+/*var prixChaussures = 70;
+var prixJean = 59;
+var prixTshirt = 20;
+var total = prixChaussures + prixJean + prixTshirt;
+console.log("Le total sans réduction est de " + total + " euros.");
+var reduction = 20/100;
+var totalReduction = total*(1 - reduction);
+console.log("Le total avec réduction est de " + totalReduction + " euros.");*/
+
+//Exo 4//
+
+/*var nombre1 = Number(prompt("Entrez la valeur du nombre 1 :"));
+var nombre2 = Number(prompt("Entrez la valeur du nombre 2 :"));
+var resultat = nombre1 + nombre2;
+console.log("La somme des 2 nombres est de " + resultat);*/
+
+//Exo 5//
+
+/*var prenom = prompt("Entrez votre prenom :");
+var nom = prompt("Entrez votre nom :");
+
+var prenomFirstLetter = prenom.substr(0, 1).toUpperCase();
+var prenomLastLetter = prenom.substr(-1, 1).toUpperCase();
+
+console.log("Votre prénom commence par la lettre " + prenomFirstLetter + " et se termine par la lettre " + prenomLastLetter);
+
+var nomFirstLetter = nom.substr(0, 1).toUpperCase();
+var nomLastLetter = nom.substr(-1, 1).toUpperCase();
+
+console.log("Votre nom commence par la lettre " + nomFirstLetter + " et se termine par la lettre " + nomLastLetter);
+
+console.log("Soit combiné : " + prenomFirstLetter + prenomLastLetter + nomFirstLetter + nomLastLetter);
+
+var age = Number(prompt("Entrez votre âge: "));
+var ageDivise = age/33;
+console.log(Math.round(ageDivise));*/
